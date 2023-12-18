4cc929fcf4ca5b27d804c9f45e0227068b6becac
Stanislaw Skalkovich <stas.skalkovich@gmail.com>
2023-12-18 13:39:27 +0300

    feat: add html to mobile menu

---
+++ Stage 3.2 - Javascript. DOM/Spring Copy/scripts/mobile_menu.js
@@ -0,0 +1,5 @@
+const closeMenuButton = document.querySelector('#close-mobile-menu');
+const openMenuButton = document.querySelector('#open-mobile-menu');
+
+closeMenuButton.addEventListener("click",()=>{})
+openMenuButton.addEventListener("click",()=>{})
